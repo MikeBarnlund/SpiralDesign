276d1d3eff16ed3bf011498184472ef1b50fa5f0
Mike Barnlund <mike.barnlund@gmail.com>
2011-10-25 22:55:25 -0600

    Initial Wordpress Installation

---
+++ website/wp-content/themes/sdbase/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div id="post-0" class="post error404 not-found">
+	<h1 class="entry-title"><?php _e( 'Not Found', 'sdbase' ); ?></h1>
+	<div class="entry-content">
+		<p><?php _e( 'Sorry, that page doesn\'t exist here. ', 'sdbase' ); ?></p>
+	</div>
+</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
