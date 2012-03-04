43d646bfaf93bf808f113ddf069aad2c44b088b4
Mike Barnlund <mike.barnlund@gmail.com>
2012-03-04 10:12:59 -0700

    About page and css cleanup

---
+++ website/wp-content/themes/iandi/about.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Template Name: About
+*/
+
+get_header();
+
+the_post();
+
+?>
+
+<div class="content">
+	<?php if ( get_the_ID() ) { ?>
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<div class="interview-header">
+				<?php $image = wp_get_attachment_image_src(get_field('feature_image'), 'full'); ?>
+				<img src="<?php echo $image[0]; ?>" alt="<?php get_the_title(get_field('feature_image')) ?>" />
+
+				<h2 class="entry-title">
+					Asher Compton
+				</h2>
+
+				<h3>
+					Of Industry and Interest
+				</h3>
+
+				<div class="summary">
+					<?php the_content(); ?>
+					<?php edit_post_link( __( 'Edit', 'iandi' ), '<div class="edit-link">', '</div>' ) ?>
+				</div>
+
+				<div class="social">
+					<!-- Put in twitter / facebook sprites -->
+				</div>
+			</div>
+			<?php
+			$links = get_field( 'recommended_links' );
+			if( $links ) { ?>
+				<h1>Recommended</h1>
+				<ul>
+				<?php foreach( $links as $link ) {
+					echo '<li><a target="_blank" href="' . $link['link_address'] . '">' . $link['link_text'] . '</a></li>';
+				} ?>
+				</ul>
+			<?php } ?>
+		</div><!-- #post-<?php the_ID(); ?> -->
+	<?php } ?>
+</div>
+
+<?php
+
+get_footer();
+
+?>
--- website/wp-content/themes/iandi/single-interview.php
+++ website/wp-content/themes/iandi/single-interview.php
@@ -15,18 +15,14 @@ the_post();
 			<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'iandi' ) . '&after=</div>') ?>
 		</div><!-- .entry-content -->
 		<?php
-		if ( post_custom('links') ) { ?>
-			<div class="links">
-				<h3>Check Out:</h3>
-				<ul>
-					<?php
-					$links = explode('<br />', get_field( 'links' ) );
-					foreach ( $links as $link ) {
-						echo '<li><a href="' . $link . '">' . $link . '</a></li>';
-					}
-					?>
-				</ul>
-			</div>
+		$links = get_field( 'recommended_links' );
+		if( $links ) { ?>
+			<h1>Recommended</h1>
+			<ul>
+			<?php foreach( $links as $link ) {
+				echo '<li><a target="_blank" href="' . $link['link_address'] . '">' . $link['link_text'] . '</a></li>';
+			} ?>
+			</ul>
 		<?php } ?>
 	</div><!-- #post-<?php the_ID(); ?> -->
 	<?php } ?>
