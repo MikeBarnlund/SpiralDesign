fc4e6fb99e5329a3c58d27d42c0354594f1ac6f9
Mike Barnlund <mike.barnlund@gmail.com>
2012-03-03 19:07:57 -0700

    Cleanup

--- website/wp-content/themes/vv/footer.php
+++ website/wp-content/themes/vv/footer.php
@@ -3,11 +3,7 @@
 	</div> <!-- end #contentwrapper -->
 	<div id="footer">
 		<div class="container">
-			<div class="social">
-				<ul id="twitter_updates">
-					<li>Refreshing Twitter Updates...</li>
-				</ul>
-			</div>
+
 			<a target="_blank" class="spiraldesign" href="http://www.spiraldesign.ca">&nbsp;</a>
 		</div>
 	</div>
