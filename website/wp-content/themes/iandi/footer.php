405c2d76a14136c2d25f07a62b8019b7b252752c
Mike Barnlund <mike.barnlund@gmail.com>
2012-03-04 19:03:25 -0700

    Lots of footer skinning

--- website/wp-content/themes/iandi/footer.php
+++ website/wp-content/themes/iandi/footer.php
@@ -3,8 +3,11 @@
 
 	<div id="footer">
 		<div class="container">
-
-			<a target="_blank" class="spiraldesign" href="http://www.spiraldesign.ca">&nbsp;</a>
+			<div class="logos">
+				<a class="twitter" target="_blank" href="http://www.twitter.com/vauntvisual"></a>
+				<a class="email" href="mailto:asher@industryandinterest.com"></a>
+				<a class="vv" target="_blank" href="http://vauntvisual.com"></a>
+			</div>
 		</div>
 	</div>
 <?php get_template_part( 'footer', 'bare' ); ?>
