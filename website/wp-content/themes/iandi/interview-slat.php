9b51f6b1da72fddcc4986af4fa466616db28f1fe
Mike Barnlund <mike.barnlund@gmail.com>
2012-03-08 21:49:38 -0700

    Interviews Page

--- website/wp-content/themes/iandi/interview-slat.php
+++ website/wp-content/themes/iandi/interview-slat.php
@@ -1,26 +1,31 @@
-<div id="post-<?php the_ID(); ?>" class="post editable">
+<?php
+$state = get_field( 'state' );
+$special_interview_markup = '';
+switch ( $state ) {
+	case 'upnext':
+		$special_interview_markup = '<h3 class="up-next">UP NEXT</h3>';
+		break;
+	case 'featured':
+		$special_interview_markup =  '<h3 class="featured-interview">FEATURED</h3>';
+		break;
+}
+?>
+
+<div id="post-<?php the_ID(); ?>" class="interview-slat <?php echo $state; ?> <?php echo $slat_class; ?>">
 	<?php $image = wp_get_attachment_image_src(get_field('feature_image'), 'full'); ?>
 	<img src="<?php echo $image[0]; ?>" alt="<?php get_the_title(get_field('feature_image')) ?>" />
 
 	<?php
 	$state = get_field( 'state' );
-	switch ( $state ) {
-		case 'upnext':
-			echo '<div class="up-next"><h3>UP NEXT</h3></div>';
-			break;
-		case 'featured':
-			echo '<h3 class="featured-interview">FEATURED</h3>';
-			break;
-	}
+	echo $special_interview_markup;
 	?>
 
-	<?php edit_post_link( __( 'Edit', 'iandi' ), "<div class=\"edit-link\">", "</div>" ) ?>
-
-	<h2 class="entry-title">
-		<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'iandi' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-	</h2>
-
-	<h3>
-		<?php the_field( 'interviewee_title' ); ?>
-	</h3>
+	<div class="interviewee-info">
+		<h2>
+			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'iandi' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+		</h2>
+		<h3 class="interviewee-title">
+			<?php the_field( 'interviewee_title' ); ?>
+		</h3>
+	</div>
 </div>
--- website/wp-content/themes/iandi/interviews.php
+++ website/wp-content/themes/iandi/interviews.php
@@ -9,36 +9,33 @@ get_header();
 
 <div class="content">
 	<h1 class="entry-title"><?php the_title(); ?></h1>
-	<?php
+	<div class="interviews section">
+		<?php
 
-	/* Main Loop */
+		/* Main Loop */
 
-	$query = "
-		SELECT wposts.*
-		FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
-		WHERE wposts.ID = wpostmeta.post_id
-		AND wpostmeta.meta_key = 'state'
-		AND wpostmeta.meta_value != 'upcoming'
-		AND wposts.post_status = 'publish'
-		AND wposts.post_type = 'interview'
-		ORDER BY wposts.post_date DESC
-	";
+		$query = "
+			SELECT wposts.*
+			FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
+			WHERE wposts.ID = wpostmeta.post_id
+			AND wpostmeta.meta_key = 'state'
+			AND wpostmeta.meta_value != 'upcoming'
+			AND wposts.post_status = 'publish'
+			AND wposts.post_type = 'interview'
+			ORDER BY wposts.post_date DESC
+		";
 
-	$the_posts = $wpdb->get_results( $query, OBJECT );
+		$the_posts = $wpdb->get_results( $query, OBJECT );
+		$posts_per_row = 5;
+		$i = 1;
 
-	foreach ( $the_posts as $post) {
-		setup_postdata( $post );
-		get_template_part( 'interview', 'slat' );
-	}
-
-	/* Bottom post navigation */
-
-	global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
-		<div id="nav-below" class="navigation">
-			<div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Older posts', 'iandi' )) ?></div>
-			<div class="nav-next"><?php previous_posts_link(__( 'Newer posts <span class="meta-nav">&raquo;</span>', 'iandi' )) ?></div>
-		</div>
-	<?php } ?>
+		foreach ( $the_posts as $post) {
+			setup_postdata( $post );
+			$slat_class = $i++ === $posts_per_row ? 'last' : '';
+			include( locate_template( 'interview-slat.php' ) );
+		}
+		?>
+	</div> <!-- .interviews -->
 </div> <!-- .content -->
 
 <?php
