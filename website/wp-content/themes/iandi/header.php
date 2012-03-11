1af9cbe2bd46bf2a1952ae1927332710886bee58
Mike Barnlund <mike.barnlund@gmail.com>
2012-03-10 17:08:02 -0700

    Added twitter to menu

--- website/wp-content/themes/iandi/header.php
+++ website/wp-content/themes/iandi/header.php
@@ -3,7 +3,10 @@
 		<div class="container">
 			<div id="nav">
 				<a class="logo" href="/"><img src="<?php bloginfo( 'stylesheet_directory' ) ?>/assets/img/logo.png"/>&nbsp;</a>
-				<?php wp_nav_menu( ); ?>
+				<div class="nav-menu">
+					<a class="twitter-logo" href="https://twitter.com/#!/@indandint"></a>
+					<?php wp_nav_menu( ); ?>
+				</div>
 			</div>
 			<h1>Showcasing <em>Industry</em> Leaders and Things of <em>Interest</em></h1>
 		</div>
