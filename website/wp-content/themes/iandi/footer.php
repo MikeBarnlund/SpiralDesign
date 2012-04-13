871cb70033c5abf62df00c5277ef5c3701683f58
Mike Barnlund <mike.barnlund@gmail.com>
2012-04-13 14:34:58 -0600

    Removed unlisted interviews from bottom nav

--- website/wp-content/themes/iandi/footer.php
+++ website/wp-content/themes/iandi/footer.php
@@ -27,8 +27,8 @@
 				$previous_post = get_adjacent_post( false, '', true );
 				$previous_state = get_field( 'state', $previous_post->ID );
 
-				if ( $next_state === 'upcoming' || $next_state === 'upnext' ) $next_post = '';
-				if ( $previous_state === 'upcoming' || $previous_state === 'upnext' ) $previous_post = '';
+				if ( $next_state === 'upcoming' || $next_state === 'upnext' || $next_state === 'activeunlisted' ) $next_post = '';
+				if ( $previous_state === 'upcoming' || $previous_state === 'upnext' || $previous_state === 'activeunlisted' ) $previous_post = '';
 
 				echo ( !empty( $previous_post ) )
 					?	'<a href="' . get_permalink( $previous_post->ID ) . '" class="previous-post"><em>&lt;&lt;</em> ' .
