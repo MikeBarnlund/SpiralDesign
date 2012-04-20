fc5dd0816e43a5b2e20cbc4ee125800aa8b4b600
Mike Barnlund <mike.barnlund@gmail.com>
2012-04-19 22:26:36 -0600

    branding and header

--- website/wp-content/themes/vv/header.php
+++ website/wp-content/themes/vv/header.php
@@ -1,10 +1,19 @@
 <?php get_template_part( 'header', 'bare') ; ?>
 	<div id="contentwrapper">
 		<div id="header">
-			<div id="logo"></div>
-			<div id="nav">
-				<div class="container">
-					<?php wp_nav_menu( ); ?>
+			<div class="container">
+				<img id="logo-top" src="<?php bloginfo( 'template_url' ) ?>/assets/img/logo-top.png"/>
+				<div id="nav">
+					<img id="logo" src="<?php bloginfo( 'template_url' ) ?>/assets/img/logo.png"/>
+					<div class="menu">
+						<a href="/home">Home</a>
+						<a href="/about">About Us</a>
+						<a href="/current-listings">Current Listings</a>
+						<a>MLS</a>
+						<a href="/whats-happening">What's Happening In Calgary</a>
+						<a href="/resources">Resources</a>
+						<a href="/contact">Contact Us</a>
+					</div>
 				</div>
 			</div>
 		</div>
