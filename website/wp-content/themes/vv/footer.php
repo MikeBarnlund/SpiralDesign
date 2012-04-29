fb64bb9592e0358b397b88fcae60be469f573bd3
Mike Barnlund <mike.barnlund@gmail.com>
2012-04-29 12:17:13 -0600

    Footer

--- website/wp-content/themes/vv/footer.php
+++ website/wp-content/themes/vv/footer.php
@@ -1,9 +1,11 @@
 		</div> <!-- end #contentbody -->
 	</div> <!-- end #contentwrapper -->
-	<div id="footer">
-		<div class="container">
-
-			<a target="_blank" class="spiraldesign" href="http://www.spiraldesign.ca">&nbsp;</a>
-		</div>
+	<div id="footer" class="container">
+		<em>&copy;</em> 2011 Love Calgary Real Estate  <em>|</em>  www.lovecalgary.ca  <em>|</em>  Site by vaunt visual
+		<span>
+			<a target="_blank" href="http://www.realtor.ca" title="Realtor.ca"><img src="<?php bloginfo( 'template_url' ) ?>/assets/img/logo-realtor.png"/></a>
+			<a target="_blank" href="http://www.mls.ca" title="MLS.ca"><img src="<?php bloginfo( 'template_url' ) ?>/assets/img/logo-mls.png"/></a>
+			<a target="_blank" href="http://www.maxwellrealty.ca" title="MaxWellRealty.ca"><img src="<?php bloginfo( 'template_url' ) ?>/assets/img/logo-maxwell.png"/></a>
+		</span>
 	</div>
 <?php get_template_part( 'footer', 'bare' ); ?>
