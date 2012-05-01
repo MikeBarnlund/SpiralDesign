9297d20fb7d8497325cbab796499d0f838eddec7
Mike Barnlund <mike.barnlund@gmail.com>
2012-04-30 22:34:30 -0600

    homepage ui

--- website/wp-content/themes/vv/index.php
+++ website/wp-content/themes/vv/index.php
@@ -19,36 +19,36 @@ the_post();
 	<?php } ?>
 </div>
 
-<div class="content">
-	<div class="section">
-		<h1 class="header-font">About <em>Love Calgary Real Estate</em></h1>
-		<?php the_content(); ?>
-	</div>
-
-	<div class="section">
-		<h1>Featured <em>Listings</em></h1>
-		<?php
-		$query = "
-			SELECT wposts.*
-			FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
-			WHERE wposts.ID = wpostmeta.post_id
-			AND wpostmeta.meta_key = 'state'
-			AND wpostmeta.meta_value = 'featured'
-			AND wposts.post_status = 'publish'
-			AND wposts.post_type = 'listing'
-			ORDER BY wposts.post_date DESC
-			LIMIT 3
-		";
-
-		$the_posts = $wpdb->get_results( $query, OBJECT );
-
-		foreach ( $the_posts as $post) {
-			setup_postdata( $post );
-			get_template_part( 'listing', 'slat' );
-		}
-		?>
-		<a class="all-listings" href="/current-listings">See All&nbsp;&nbsp;<em>>></em></a>
-	</div>
+<div class="entry-content thin-bottom">
+	<h1 class="header-font">About <em>Love Calgary Real Estate</em></h1>
+	<?php the_content(); ?>
 </div>
+
+<div class="entry-content-em thin-bottom">
+	<h1 class="extra-bottom-margin">Featured <em>Listings</em></h1>
+	<?php
+	$query = "
+		SELECT wposts.*
+		FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
+		WHERE wposts.ID = wpostmeta.post_id
+		AND wpostmeta.meta_key = 'state'
+		AND wpostmeta.meta_value = 'featured'
+		AND wposts.post_status = 'publish'
+		AND wposts.post_type = 'listing'
+		ORDER BY wposts.post_date DESC
+		LIMIT 3
+	";
+
+	$the_posts = $wpdb->get_results( $query, OBJECT );
+
+	foreach ( $the_posts as $post) {
+		setup_postdata( $post );
+		global $more;
+		$more = 0;
+		get_template_part( 'listing', 'slat' );
+	}
+	?>
+</div>
+
 <?php
 get_footer();
