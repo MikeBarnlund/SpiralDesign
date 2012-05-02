b1ee90c1a69ff6e68842b5f79d8e71b60e6806bd
Mike Barnlund <mike.barnlund@gmail.com>
2012-05-01 22:09:19 -0600

    Search Page

--- website/wp-content/themes/vv/searchpage.php
+++ website/wp-content/themes/vv/searchpage.php
@@ -33,12 +33,10 @@ if ( !empty( $type ) ) {
 	);
 
 	if ( $type != 'any' ) {
-		echo 'Using listing type';
 		$args['listingtype'] = $type;
 	}
 
 	if ( $price_min != '0' ) {
-		echo 'Using price_min';
 		$meta_queries[] = array(
 			'key' => 'price',
 			'value' => $price_min,
@@ -48,7 +46,6 @@ if ( !empty( $type ) ) {
 	}
 
 	if ( $price_max != 'any' ) {
-		echo 'Using price_min';
 		$meta_queries[] = array(
 			'key' => 'price',
 			'value' => $price_max,
@@ -58,12 +55,10 @@ if ( !empty( $type ) ) {
 	}
 
 	if ( $community != 'any' ) {
-		echo 'Using community';
 		$args['community'] = $community;
 	}
 
 	if ( $bedroom_min != 'any' ) {
-		echo 'Using bedroom_min';
 		$meta_queries[] = array(
 			'key' => 'bedroom',
 			'value' => $bedroom_min,
@@ -73,7 +68,6 @@ if ( !empty( $type ) ) {
 	}
 
 	if ( $bedroom_max != 'any' ) {
-		echo 'Using bedroom_max';
 		$meta_queries[] = array(
 			'key' => 'bedroom',
 			'value' => $bedroom_max,
@@ -87,98 +81,114 @@ if ( !empty( $type ) ) {
 	$query = new WP_Query( $args );
 }
 
-$dump = $query->request;
-echo $dump;
-
 ?>
 
 <div class="content">
-	<form method="post" class="search" id="searchform" action="/search">
+	<div class="entry-content search-page">
+		<h1 class="page-title">Search Listings</h1>
+		<div class="entry-content-em">
+			<form method="post" class="search" id="searchform" action="/search">
+				<div class="form-fields">
+					<label>Type:</label>
+					<div class="select-cutoff">
+						<select id="type" name="type">
+							<?php
+							echo '<option value="any">Any Listing Type</option>';
+							$terms = get_terms( 'listingtype' );
+							foreach ( $terms as $term ) {
+								$selected = ( $type == $term->slug ) ? ' selected="selected"' : '';
+								echo '<option value="' . $term->slug . '"' . $selected . '>' . $term->name . '</option>';
+							}
+							?>
+						</select>
+					</div>
 
-		<label>Type:</label>
-		<select id="type" name="type">
-			<?php
-			echo '<option value="any">Any Listing Type</option>';
-			$terms = get_terms( 'listingtype' );
-			foreach ( $terms as $term ) {
-				$selected = ( $type == $term->slug ) ? ' selected="selected"' : '';
-				echo '<option value="' . $term->slug . '"' . $selected . '>' . $term->name . '</option>';
-			}
-			?>
-		</select>
+					<label>Price Range:</label>
+					<div class="select-cutoff">
+						<select id="price_min" name="price_min">
+							<?php
+							echo '<option value="0">$0</option>';
+							$price = 100000;
+							while ( $price <= 1000000 ) {
+								$selected = ( $price_min == $price ) ? ' selected="selected"' : '';
+								echo '<option value="' . $price . '"' . $selected . '>' . money_format( '%.0n', $price ) . '</option>';
+								$price += 25000;
+							}
+							?>
+						</select>
+					</div>
+					<span>to</span>
+					<div class="select-cutoff">
+						<select id="price_max" name="price_max">
+							<?php
+							echo '<option value="any">Any</option>';
+							$price = 100000;
+							while ( $price <= 1000000 ) {
+								$selected = ( $price_max == $price ) ? ' selected="selected"' : '';
+								echo '<option value="' . $price . '"' . $selected . '>' . money_format( '%.0n', $price ) . '</option>';
+								$price += 25000;
+							}
+							?>
+						</select>
+					</div>
 
-		<label>Price Range:</label>
-		<select id="price_min" name="price_min">
-			<?php
-			echo '<option value="0">$0</option>';
-			$price = 100000;
-			while ( $price <= 1000000 ) {
-				$selected = ( $price_min == $price ) ? ' selected="selected"' : '';
-				echo '<option value="' . $price . '"' . $selected . '>' . money_format( '%.0n', $price ) . '</option>';
-				$price += 25000;
-			}
-			?>
-		</select>
-		to
-		<select id="price_max" name="price_max">
-			<?php
-			echo '<option value="any">Any</option>';
-			$price = 100000;
-			while ( $price <= 1000000 ) {
-				$selected = ( $price_max == $price ) ? ' selected="selected"' : '';
-				echo '<option value="' . $price . '"' . $selected . '>' . money_format( '%.0n', $price ) . '</option>';
-				$price += 25000;
-			}
-			?>
-		</select>
+					<label>Community:</label>
+					<div class="select-cutoff">
+						<select id="community" name="community">
+							<?php
+							echo '<option value="any">Any Community</option>';
+							$terms = get_terms( 'community' );
+							foreach ( $terms as $term ) {
+								$selected = ( $community == $term->slug ) ? ' selected="selected"' : '';
+								echo '<option value="' . $term->slug . '"' . $selected . '>' . $term->name . '</option>';
+							}
+							?>
+						</select>
+					</div>
 
-		<label>Community:</label>
-		<select id="community" name="community">
-			<?php
-			echo '<option value="any">Any Community</option>';
-			$terms = get_terms( 'community' );
-			foreach ( $terms as $term ) {
-				$selected = ( $community == $term->slug ) ? ' selected="selected"' : '';
-				echo '<option value="' . $term->slug . '"' . $selected . '>' . $term->name . '</option>';
-			}
-			?>
-		</select>
-
-		<label>No. Of Bedrooms:</label>
-		<select id="bedroom_min" name="bedroom_min">
-			<?php
-			echo '<option value="any">Any</option>';
-			for ( $i = 1; $i < 8; $i++ ) {
-				$selected = ( $bedroom_min == $i ) ? ' selected="selected"' : '';
-				echo '<option value="' . $i . '"' . $selected . '>' . $i . '</option>';
-			}
-			?>
-		</select>
-		to
-		<select id="bedroom_max" name="bedroom_max">
-			<?php
-			echo '<option value="any">Any</option>';
-			for ( $i = 1; $i < 8; $i++ ) {
-				$selected = ( $bedroom_max == $i ) ? ' selected="selected"' : '';
-				echo '<option value="' . $i . '"' . $selected . '>' . $i . '</option>';
-			}
-			?>
-		</select>
-
-		<button type="submit" class="submit" name="submit" id="searchsubmit">Submit</button>
-	</form>
-
-	<?php
-	if ( !empty( $query ) ) { ?>
-		<div class="section">
-			<?php
-			// The Loop
-			while ( $query->have_posts() ) : $query->the_post();
-				get_template_part( 'listing', 'slat' );
-			endwhile;
-			?>
+					<label>No. Of Bedrooms:</label>
+					<div class="select-cutoff">
+						<select id="bedroom_min" name="bedroom_min">
+							<?php
+							echo '<option value="any">Any</option>';
+							for ( $i = 1; $i < 8; $i++ ) {
+								$selected = ( $bedroom_min == $i ) ? ' selected="selected"' : '';
+								echo '<option value="' . $i . '"' . $selected . '>' . $i . '</option>';
+							}
+							?>
+						</select>
+					</div>
+					<span>to</span>
+					<div class="select-cutoff">
+						<select id="bedroom_max" name="bedroom_max">
+							<?php
+							echo '<option value="any">Any</option>';
+							for ( $i = 1; $i < 8; $i++ ) {
+								$selected = ( $bedroom_max == $i ) ? ' selected="selected"' : '';
+								echo '<option value="' . $i . '"' . $selected . '>' . $i . '</option>';
+							}
+							?>
+						</select>
+					</div>
+				</div>
+				<button type="submit" class="submit" name="submit" id="searchsubmit">Submit</button>
+			</form>
 		</div>
-	<?php } ?>
+
+		<?php
+		if ( !empty( $query ) ) { ?>
+			<div class="section">
+				<?php
+				// The Loop
+				while ( $query->have_posts() ) : $query->the_post();
+					get_template_part( 'listing', 'slat' );
+				endwhile;
+				?>
+			</div>
+		<?php } else { ?>
+			<div class="no-listings">Sorry, no listings matched your search.</div>
+		<?php } ?>
+	</div>
 </div>
 
 <?php
