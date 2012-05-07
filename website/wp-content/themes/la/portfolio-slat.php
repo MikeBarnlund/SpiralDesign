c34b5bd17089c5f9b1bebfe4d81b284d456b8014
Mike Barnlund <mike.barnlund@gmail.com>
2012-05-07 01:05:20 -0600

    Portfolio selection skinning

--- website/wp-content/themes/la/portfolio-slat.php
+++
@@ -1,6 +0,0 @@
-<div class='fuck'>
-	<?php
-	$background_image_url = get_field( 'portfolio-link-background' );
-	?>
-	<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-</div>
