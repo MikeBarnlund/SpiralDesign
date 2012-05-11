c20604543bdb16940577919c6ef3808ada784787
Mike Barnlund <mike.barnlund@gmail.com>
2012-05-10 19:47:33 -0600

    final lap updates

--- website/wp-content/themes/la/header.php
+++ website/wp-content/themes/la/header.php
@@ -1,6 +1,6 @@
 <?php get_template_part( 'header', 'bare') ; ?>
 	<div id="page">
 		<header class="clearfix">
-			<div id="logo"></div>
+			<a href="/" id="logo"></a>
 		</header>
 		<div role="main">
