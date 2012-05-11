4dd3e53606bf1cd415c8e2c80d366309321a2c19
Mike Barnlund <mike.barnlund@gmail.com>
2012-05-10 18:52:42 -0600

    Numerous feedback changes

--- website/wp-content/themes/la/home.php
+++ website/wp-content/themes/la/home.php
@@ -34,7 +34,15 @@ $( document ).ready( function() {
         echo ' ]; ';
         ?>
         if ( imgList ) {
-            $( '.slideshow' ).sdslideshow( { imageList: imgList, transitionDelay: 6000, transitionDuration: 1000 } );
+            var settings = {
+                imageList: imgList,
+                transitionDelay: 6000,
+                transitionDuration: 1000
+            };
+            if ( Modernizr.mq( 'only screen and (max-width: 480px)' ) ) {
+                settings.transitionDelay = 3000;
+            }
+            $( '.slideshow' ).sdslideshow( settings );
         }
         <?php
     }
