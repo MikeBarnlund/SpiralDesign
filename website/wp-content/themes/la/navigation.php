9698b68e1127f8e746b1035c8e8366992026f503
Mike Barnlund <mike.barnlund@gmail.com>
2012-05-11 01:59:18 -0600

    Fixed nav

--- website/wp-content/themes/la/navigation.php
+++ website/wp-content/themes/la/navigation.php
@@ -2,13 +2,24 @@
     <img src="<?php bloginfo( 'template_url' ) ?>/assets/img/info-inactive.png" />
 </div>
 <nav>
-    <div class="nav-item"><a href="/portfolio">Portfolio</a></div>
+    <?php
+    $navitem1 = null;
+    $navitem2 = null;
+    if ( is_home() || is_front_page() ) {
+        $navitem1 = array ( 'title' => 'Portfolio', 'url' => '/portfolio' );
+        $navitem2 = array ( 'title' => 'Contact', 'url' => '/contact' );
+    } else if ( is_page( 'contact' ) ) {
+        $navitem1 = array ( 'title' => 'Home', 'url' => '/' );
+        $navitem2 = array ( 'title' => 'Portfolio', 'url' => '/portfolio' );
+    }
+    ?>
+    <div class="nav-item"><a href="<?php echo $navitem1['url']; ?>"><?php echo $navitem1['title']; ?></a></div>
     <div class="portfolio-nav clearfix">
         <a class="portfolio-prev"><img src="<?php bloginfo( 'template_url' ) ?>/assets/img/nav-left.png" /></a>
         <img class="logo" src="<?php bloginfo( 'template_url' ) ?>/assets/img/logo.png" />
         <a class="portfolio-next"><img src="<?php bloginfo( 'template_url' ) ?>/assets/img/nav-right.png" /></a>
     </div>
-    <div class="nav-item"><a href="/contact">Contact</a></div>
+    <div class="nav-item"><a href="<?php echo $navitem2['url']; ?>"><?php echo $navitem2['title']; ?></a></div>
 </nav>
 <div class="redbar">
     <div class="info-logo"></div>
