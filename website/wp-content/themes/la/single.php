521a60a88a697c30b184d9d9fe72f6756a574620
Mike Barnlund <mike.barnlund@gmail.com>
2012-05-12 17:01:24 -0600

    Categories Page

--- website/wp-content/themes/la/single.php
+++ website/wp-content/themes/la/single.php
@@ -2,18 +2,49 @@
 
 get_header();
 
-/* Main Loop */
+$category = get_the_category();
+$slug = ( $category[0]->slug ) ? $category[0]->slug : 'uncategorized';
+$name = ( $category[0]->name ) ? $category[0]->name : 'uncategorized';
 
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'post' );
-endwhile;
+?>
+<div class="single-post">
+    <div class="logo logo-full <?php echo $slug; ?>">
+        <img src="<?php bloginfo( 'template_url' ) ?>/assets/img/tcd-logo-post.png" />
+        <div class="category-label"><?php echo $name; ?></div>
+        <div class="archive-label tk-myriad-pro"><img src="<?php bloginfo( 'template_url' ) ?>/assets/img/archive-label-bg.png">Archive</div>
+        <div class="logo-bottom"></div>
+    </div>
+    <?php
 
-next_post_link( format, link, in_same_cat, excluded_categories );
+    /* Main Loop */
 
-previous_post_link( '%link', __( '<span class="meta-nav">←</span> Previous', 'twentyeleven' ) );
-echo ' | ';
-next_post_link( '%link', __( 'Next <span class="meta-nav">→</span>', 'twentyeleven' ) );
+    while ( have_posts() ) :
+    	the_post();
+    	get_template_part( 'post' );
+    endwhile;
+
+    ?>
+    <div class="post-navigation">
+        <div class="next-prev">
+            <a class="previous-post">Previous</a>
+            <img src="<?php bloginfo( 'template_url' ) ?>/assets/img/post-navigation-divider.png" />
+            <a class="next-post">Next</a>
+        </div>
+        <div class="post-names">
+
+        </div>
+    </div>
+    <?php
+
+    //next_post_link( format, link, in_same_cat, excluded_categories );
+
+    previous_post_link( '%link', __( '<span class="meta-nav">←</span> Previous', 'twentyeleven' ) );
+    echo ' | ';
+    next_post_link( '%link', __( 'Next <span class="meta-nav">→</span>', 'twentyeleven' ) );
+
+    ?>
+</div>
+<?php
 
 get_footer();
 
