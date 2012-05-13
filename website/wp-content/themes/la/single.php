9c702dc1a1f71070cfc38731c3d0b825f099c554
Mike Barnlund <mike.barnlund@gmail.com>
2012-05-12 20:59:50 -0600

    Wordpress media cleanup

--- website/wp-content/themes/la/single.php
+++ website/wp-content/themes/la/single.php
@@ -32,44 +32,44 @@ $name = ( $category[0]->name ) ? $category[0]->name : 'uncategorized';
 	$next_post = get_adjacent_post( true, '', false );
 	$previous_post = get_adjacent_post( true, '', true );
 
-	?>
+	if ( !empty( $next_post ) || !empty( $previous_post ) ) { ?>
+        <div class="post-navigation">
+            <div class="next-prev">
+                <?php
 
-    <div class="post-navigation">
-        <div class="next-prev">
-            <?php
+        		echo ( !empty( $previous_post ) )
+        			?	'<a href="' . get_permalink( $previous_post->ID ) . '" class="previous-post">Previous</a>'
+        			: '<div class="post-placeholder"></div>';
+        		echo '<img src="' . get_bloginfo( 'template_url' ) . '/assets/img/post-navigation-divider.png" />';
+        		echo ( !empty( $next_post ) )
+        			?	'<a href="' . get_permalink( $next_post->ID ) . '" class="next-post">Next</a>'
+        			: '<div class="post-placeholder"></div>';
 
-    		echo ( !empty( $previous_post ) )
-    			?	'<a href="' . get_permalink( $previous_post->ID ) . '" class="previous-post">Previous</a>'
-    			: '<div class="post-placeholder"></div>';
-    		echo '<img src="' . get_bloginfo( 'template_url' ) . '/assets/img/post-navigation-divider.png" />';
-    		echo ( !empty( $next_post ) )
-    			?	'<a href="' . get_permalink( $next_post->ID ) . '" class="next-post">Next</a>'
-    			: '<div class="post-placeholder"></div>';
+                ?>
+            </div>
+            <div class="post-names">
+                <?php
 
-            ?>
+        		echo ( !empty( $previous_post ) )
+        			?	'<a href="' . get_permalink( $previous_post->ID ) . '" class="previous-post">' .
+        				( strlen( $previous_post->post_title ) > $title_maxlen
+        					? substr( $previous_post->post_title, 0, $title_maxlen ) . '...'
+        					: $previous_post->post_title ) .
+        				'</a>'
+        			: '<div class="post-placeholder"></div>';
+        		echo '<img src="' . get_bloginfo( 'template_url' ) . '/assets/img/post-navigation-divider.png" />';
+        		echo ( !empty( $next_post ) )
+        			?	'<a href="' . get_permalink( $next_post->ID ) . '" class="next-post">' .
+        				( strlen( $next_post->post_title ) > $title_maxlen
+        					? substr( $next_post->post_title, 0, $title_maxlen ) . '...'
+        					: $next_post->post_title ) .
+        				'</a>'
+        			: '<div class="post-placeholder"></div>';
+
+                ?>
+            </div>
         </div>
-        <div class="post-names">
-            <?php
-
-    		echo ( !empty( $previous_post ) )
-    			?	'<a href="' . get_permalink( $previous_post->ID ) . '" class="previous-post">' .
-    				( strlen( $previous_post->post_title ) > $title_maxlen
-    					? substr( $previous_post->post_title, 0, $title_maxlen ) . '...'
-    					: $previous_post->post_title ) .
-    				'</a>'
-    			: '<div class="post-placeholder"></div>';
-    		echo '<img src="' . get_bloginfo( 'template_url' ) . '/assets/img/post-navigation-divider.png" />';
-    		echo ( !empty( $next_post ) )
-    			?	'<a href="' . get_permalink( $next_post->ID ) . '" class="next-post">' .
-    				( strlen( $next_post->post_title ) > $title_maxlen
-    					? substr( $next_post->post_title, 0, $title_maxlen ) . '...'
-    					: $next_post->post_title ) .
-    				'</a>'
-    			: '<div class="post-placeholder"></div>';
-
-            ?>
-        </div>
-    </div>
+	<?php } ?>
 </div>
 <?php
 
