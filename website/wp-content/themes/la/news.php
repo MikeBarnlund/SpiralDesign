145d255c0fb809dd933e6a1ba570ce8557d3d1fa
Mike Barnlund <mike.barnlund@gmail.com>
2012-05-14 00:04:09 -0600

    Prod sync and GA

---
+++ website/wp-content/themes/la/news.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: News
+*/
+
+get_header();
+
+?>
+
+<div class="content">
+	<h1 class="entry-title"><?php the_title(); ?></h1>
+	<?php
+
+	/* Main Loop */
+
+	//omit category "Home Page Only"
+	//$the_posts = query_posts( 'posts_per_page=3' );
+	$the_posts = get_posts( );
+
+	foreach ( $the_posts as $post) {
+		setup_postdata( $post );
+		get_template_part( 'post' );
+	}
+
+	/* Bottom post navigation */
+
+	global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
+		<div id="nav-below" class="navigation">
+			<div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Older posts', 'sdbase' )) ?></div>
+			<div class="nav-next"><?php previous_posts_link(__( 'Newer posts <span class="meta-nav">&raquo;</span>', 'sdbase' )) ?></div>
+		</div>
+	<?php } ?>
+</div> <!-- .content-with-sidebar -->
+
+<?php
+
+get_footer();
+get_template_part( 'footer', 'bare' );
+
+?>
