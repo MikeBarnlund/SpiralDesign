6e27b8a527e1a3b20e4766090c929e0ce94553a8
Mike Barnlund <mike.barnlund@gmail.com>
2012-06-21 08:29:11 -0600

    Updated footer

--- website/wp-content/themes/vv/footer.php
+++ website/wp-content/themes/vv/footer.php
@@ -1,7 +1,7 @@
 		</div> <!-- end #contentbody -->
 	</div> <!-- end #contentwrapper -->
 	<div id="footer" class="container">
-		<em>&copy;</em> 2011 Love Calgary Real Estate  <em>|</em>  www.lovecalgary.ca  <em>|</em>  Site by vaunt visual
+		<em>&copy;</em> 2012 Love Calgary Real Estate  <em>|</em>  www.lovecalgary.ca  <em>|</em>  Site by <a target="_blank" title="Vaunt Visual" href="http://vauntvisual.com">vaunt visual</a>
 		<span>
 			<a target="_blank" href="http://www.realtor.ca" title="Realtor.ca"><img src="<?php bloginfo( 'template_url' ) ?>/assets/img/logo-realtor.png"/></a>
 			<a target="_blank" href="http://www.mls.ca" title="MLS.ca"><img src="<?php bloginfo( 'template_url' ) ?>/assets/img/logo-mls.png"/></a>
