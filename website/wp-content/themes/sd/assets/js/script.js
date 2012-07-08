9bdde5090ec627199c922d60f7e60c56f88d2e68
mikebarnlund <mike@chaordix.com>
2012-07-08 15:07:35 -0600

    Menu JS

--- website/wp-content/themes/sd/assets/js/script.js
+++ website/wp-content/themes/sd/assets/js/script.js
@@ -15,5 +15,19 @@ $( document ).ready( function() {
 	
 	// ============================== Menu =================================
 	
-
+    $( 'li.has-sub-menu').click( function() {
+        $submenu = $( this ).children( 'ul.sub-menu' );
+        
+        if ( !$submenu.is( ':visible' ) ) {
+            $submenu.fadeIn( 75, function() {
+                $( document ).one( 'click', function() {
+                    $submenu.fadeOut( 100 );
+                } );
+            } );
+        } else 
+            $submenu.fadeOut( 100 );
+            
+        // don't follow the link
+        return false;
+    } );
 } );
