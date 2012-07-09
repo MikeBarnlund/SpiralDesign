bf69db960036b0bde2577d8d41cab6032a26b1c7
mikebarnlund <mike.barnlund@gmail.com>
2012-07-08 19:18:14 -0600

    Added searching

--- website/wp-content/themes/sd/header.php
+++ website/wp-content/themes/sd/header.php
@@ -4,6 +4,7 @@
 			<div id="logo"></div>
 			<nav>
 				<?php wp_nav_menu( array( 'walker' => new SH_Last_Walker(), 'depth' => 0 ) ); ?>
+				<?php get_search_form(); ?>
 			</nav>
 		</header>
 		<div role="main">
---
+++ website/wp-content/themes/sd/searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div class="clearfix">
+        <input type="text" value="" name="s" id="s" placeholder="Search" />
+        <button type="submit">search</button>
+    </div>
+</form>
