cbb7bea4752d8a6a5297255091268116b87bb648
MikeBarnlund <mike.barnlund@gmail.com>
2012-07-08 20:57:17 -0600

    Twitter URL fix

--- website/wp-content/themes/sd/share.php
+++ website/wp-content/themes/sd/share.php
@@ -1,10 +1,9 @@
 <div class="share">
 	<?php
+	$tinyurl = getTinyUrl( get_permalink() );
 	$message = '"' . get_the_title() . '" on ' . get_bloginfo( 'name' );
-	$twitter_message = $message . ' - ' . getTinyUrl( get_permalink() );
 	$message = urlencode( $message );
-	$twitter_message = urlencode( $twitter_message );
 	?>
-	<span><a id="tweet_button" target="_blank" href="https://twitter.com/intent/tweet?original_referer=<?php echo urlencode( get_permalink() ); ?>&text=<?php echo $twitter_message; ?>&url=<?php echo urlencode( get_permalink() ); ?>">Twitter</a></span>
+	<span><a id="tweet_button" target="_blank" href="https://twitter.com/intent/tweet?original_referer=<?php echo urlencode( get_permalink() ); ?>&text=<?php echo $message; ?>&url=<?php echo urlencode( $tinyurl ); ?>">Twitter</a></span>
 	<span><a id="fb_button" target="_blank" href="http://www.facebook.com/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>">Facebook</a></span>
 </div>
