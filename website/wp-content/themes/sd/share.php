169be650df40f2fd9ce62b40961dde773cef2eeb
MikeBarnlund <mike.barnlund@gmail.com>
2012-07-08 21:02:14 -0600

    keychain test

--- website/wp-content/themes/sd/share.php
+++ website/wp-content/themes/sd/share.php
@@ -3,6 +3,7 @@
 	$tinyurl = getTinyUrl( get_permalink() );
 	$message = '"' . get_the_title() . '" on ' . get_bloginfo( 'name' );
 	$message = urlencode( $message );
+	$message = urlencode( $message );
 	?>
 	<span><a id="tweet_button" target="_blank" href="https://twitter.com/intent/tweet?original_referer=<?php echo urlencode( get_permalink() ); ?>&text=<?php echo $message; ?>&url=<?php echo urlencode( $tinyurl ); ?>">Twitter</a></span>
 	<span><a id="fb_button" target="_blank" href="http://www.facebook.com/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>">Facebook</a></span>
