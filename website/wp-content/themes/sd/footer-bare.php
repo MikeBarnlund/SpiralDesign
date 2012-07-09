a9a7d26093222974d9bac22701666c4cf7989207
mikebarnlund <mike.barnlund@gmail.com>
2012-07-08 20:23:29 -0600

    Added GA

--- website/wp-content/themes/sd/footer-bare.php
+++ website/wp-content/themes/sd/footer-bare.php
@@ -7,6 +7,13 @@
 	<script src="<?php bloginfo( 'stylesheet_directory' ) ?>/assets/js/plugins.js"></script>
     <script src="<?php bloginfo( 'stylesheet_directory' ) ?>/assets/js/script.js"></script>
 
+	<script>
+	// var _gaq=[['_setAccount','UA-31672706-1'],['_trackPageview']];
+	// (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	// g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	// s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
+
 	<?php wp_footer(); ?>
 	</body>
 </html>
