a09ef8db9edfef544e30707562ba4670c39c2ca7
MikeBarnlund <mike.barnlund@gmail.com>
2013-01-06 20:17:08 -0700

    Homepage slides

--- website/wp-content/themes/sd/homepage.php
+++ website/wp-content/themes/sd/homepage.php
@@ -6,12 +6,28 @@ Template Name: Home
 
 get_header();
 
-the_post();
+//the_post();
 
 ?>
-<div class="home-banner">
-	<h1>Lorem ipsum dolor<br/>sit amet</h1>
-</div>
+
+<?php
+
+$args = array( 'post_type' => 'homepageslide' );
+$loop = new WP_Query( $args );
+while ( $loop->have_posts() ) : $loop->the_post();
+	?>
+	<div class="home-banner" style="background-image: url(<?php the_field('image'); ?>)">
+		<div class="banner-content">
+			<h1><?php the_title(); ?></h1>
+			<div class="summary"><p><?php the_field('summary'); ?></p></div>
+			<div class="next-button"><a>&gt;</a></div>
+		</div>
+	</div>
+	<?php
+endwhile;
+
+?>
+
 <div class="content">
 	
 </div>
