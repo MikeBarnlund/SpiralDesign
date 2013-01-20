6b2123272374261f820af11c090b02d9ef629d09
MikeBarnlund <mike.barnlund@gmail.com>
2013-01-20 14:20:03 -0700

    contact page

--- website/wp-content/themes/sd/sidebar.php
+++ website/wp-content/themes/sd/sidebar.php
@@ -3,7 +3,7 @@
 		<div class="widget">
 			<h3>Get In Touch</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
-			<a class="button">Request Info &raquo;</a>
+			<a href="/contact" class="button">Request Info &raquo;</a>
 		</div>
 		<?php
 		
