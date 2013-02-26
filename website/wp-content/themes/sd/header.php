63ec8011a2fd79aca436b999ba39936becf27abd
MikeBarnlund <mike.barnlund@gmail.com>
2013-02-25 23:35:47 -0700

    IE8 and sort of IE7 compatibility

--- website/wp-content/themes/sd/header.php
+++ website/wp-content/themes/sd/header.php
@@ -1,7 +1,7 @@
 <?php get_template_part( 'header', 'bare') ; ?>
 	<div id="page">
 		<header>
-			<a href="/"><img id="logo" src="<?php bloginfo( 'stylesheet_directory' ) ?>/assets/img/logo.png" /></a>
+			<a id="logo" href="/"><img src="<?php bloginfo( 'stylesheet_directory' ) ?>/assets/img/logo.png" /></a>
 			<nav>
 				<?php wp_nav_menu( array( 'walker' => new SH_Last_Walker(), 'depth' => 0 ) ); ?>
 			</nav>
