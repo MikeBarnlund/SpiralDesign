62d18fe234fec9224eb48ed29dc9bfd38c9b5c4a
MikeBarnlund <mike.barnlund@gmail.com>
2013-06-02 15:12:01 -0600

    Updated JS libs and Bootstrap

--- website/wp-content/themes/sd/footer-bare.php
+++ website/wp-content/themes/sd/footer-bare.php
@@ -1,7 +1,7 @@
     <!-- Load Global Scripts -->
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ) ?>/assets/js/libs/jquery-1.9.1.min.js"><\/script>')</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ) ?>/assets/js/libs/jquery-1.10.1.min.js"><\/script>')</script>
 
     <!-- Load Theme Scripts -->
 	<script src="<?php bloginfo( 'stylesheet_directory' ) ?>/assets/js/plugins.js"></script>
